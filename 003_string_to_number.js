9a70cd0578134a3ab47071e3c5cdc02f3d978967
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-05 19:03:33 +0900

    [프로그래머스] 부족한 금액 계산하기

--- 003_string_to_number.js
+++ 003_string_to_number.js
@@ -1,7 +1,19 @@
-function solution(s) {
-  return +s;
+function solution(a) {
+  if(typeof(a) === "string"){
+    return +a;
+  } else {
+    return '바보'
+  }
 }
 
+function solution2(a) {
+  return +a;
+}
 
+function solution3(a) {
+  return parseInt(a)
+}
 
-console.log(solution('-1235'))
+console.log(solution(24))
+console.log(solution2(24))
+console.log(solution3(24))
