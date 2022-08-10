05c8b023884652448d357e5628d1491fa0dc6bb0
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-10 18:23:33 +0900

    [프로그래머스] 없는 숫자 더하기

--- 016_없는_숫자_더하기.js
+++ 016_없는_숫자_더하기.js
@@ -17,3 +17,6 @@ console.log(solution([1, 2, 3, 4, 6, 7, 8, 0]))
 function solution2(numbers) {
     return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
 }
+
+// Array.prototype.reduce() -> 배열의 각 요소에 대해 주어진 리듀서(reducer) 함수를 실행하고, 하나의 결과값을 반환
+// 누산기 (acc) / 현재 값 (cur) / 현재 인덱스 (idx) / 원본 배열 (src)
