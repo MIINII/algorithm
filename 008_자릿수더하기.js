9ec28a58c062a19801f72f09d3bdcd58ed5278d4
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-11 21:45:22 +0900

    [프로그래머스] 자릿수더하기

--- 008_자릿수더하기.js
+++ 008_자릿수더하기.js
@@ -1,17 +1,15 @@
-
 // 01. 숫자르 배열로 만든 후 각 배열의 요소를 더하기!!(두가지 방법)
 // 01 -1. 숫자를 문자로 반환 후 배열로 만들기(split)
 // 01 -2. 숫자를 do.while문을 이용하여 배열로 만들기
 
-function solution(n) { () => n = (n + '').split('')
+function solution(n) {
+  ;() => (n = (n + '').split(''))
   var answer = 0
-
-  
 
   return answer
 }
 
-// * 함수 선언문 vs 함수 표현식 
+// * 함수 선언문 vs 함수 표현식
 // 함수 선언문과 함수 표현식의 가장 큰 차이는 호출.
 // 선언문은 어디에 작성하더라도, 심지어 호출보다 아래에 작성하더라도 동작
 // 표현식은 반드시 위에서 아래로라는 순서를 따름. 호출 전에 반드시 작성되어 있어야 작동
@@ -37,14 +35,14 @@ function ask(question, yes, no) {
 }
 
 function showOk() {
-  alert("동의 완료");
+  alert('동의 완료')
 }
 
 function showCancel() {
-  alert("취소");
+  alert('취소')
 }
 
-ask("동의하십니까?", showOk, showCancel);
+ask('동의하십니까?', showOk, showCancel)
 
 // ################################################################
 // [일단 화살표 함수 사용하는 방법을 알아볼것]
@@ -63,7 +61,7 @@ const names = ['오리', '말', '소', '닭']
 
 // 03. 진짜 더 좋은 방법 (화살표 함수 중 implicit return) / {} 추가하면 implicit가 사라지니 주의하세용!
 
-const hearts = names.map((item) => item + '💞')
+const hearts = names.map(item => item + '💞')
 
 console.log(hearts) // [ '오리💞', '말💞', '소💞', '닭💞' ]
 
@@ -89,15 +87,15 @@ function solution(n) {
   for (let i = 0; i <= num.length; i++) {
     answer += parseInt(num[i]) // parseInt -> 정수로!
   }
-  
+
   return answer
   // console.log(num.length)
 }
 
 console.log(solution(123))
 
-function solution2(n){ 
-  let arr = n+(' ').split(' ').map(num => parseInt(num))
+function solution2(n) {
+  let arr = n + ' '.split(' ').map(num => parseInt(num))
   return arr.reduce((acc, cur) => acc + cur)
 }
 
